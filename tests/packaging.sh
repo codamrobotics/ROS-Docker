60180edabbcc8c30fb227d27272807a805b93576
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-02-10 11:26:22 +0100

    bump

--- tests/packaging.sh
+++ tests/packaging.sh
@@ -3,7 +3,7 @@
 # quit on first error
 set -e
 
-./ros-docker.sh --dryrun -r package ~/Projects/ros_packages "catkin_make"
-./ros-docker.sh --dryrun -c -r package ~/Projects/ros_packages "catkin_make"
-./ros-docker.sh --dryrun -c package ~/Projects/ros_packages "ls"
-./ros-docker.sh --dryrun -d package ~/Projects/ros_packages "catkin_make"
+./ros-docker.sh --dryrun -r package /tmp "catkin_make"
+./ros-docker.sh --dryrun -c -r package /tmp "catkin_make"
+./ros-docker.sh --dryrun -c package /tmp "ls"
+./ros-docker.sh --dryrun -d package /tmp "catkin_make"
--- tests/workspacing.sh
+++ tests/workspacing.sh
@@ -3,7 +3,7 @@
 # quit on first error
 set -e
 
-./ros-docker.sh --dryrun -r workspace /opt/ros/catkin_ws "catkin_make"
-./ros-docker.sh --dryrun -c -r workspace /opt/ros/catkin_ws "catkin_make"
-./ros-docker.sh --dryrun -c workspace /opt/ros/catkin_ws "ls"
-./ros-docker.sh --dryrun -d workspace /opt/ros/catkin_ws "catkin_make"
+./ros-docker.sh --dryrun -r workspace /tmp "catkin_make"
+./ros-docker.sh --dryrun -c -r workspace /tmp "catkin_make"
+./ros-docker.sh --dryrun -c workspace /tmp "ls"
+./ros-docker.sh --dryrun -d workspace /tmp "catkin_make"
