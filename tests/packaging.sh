d835192871ff7e8e8d99af0a39d00a4df942ce0d
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-02-10 18:11:11 +0100

    Added more tests for workspace/package to increase coverage Extended ros-docker.sh Github build tests

--- tests/packaging.sh
+++ tests/packaging.sh
@@ -3,7 +3,22 @@
 # quit on first error
 set -e
 
-./ros-docker.sh --dryrun -r package /tmp "catkin_make"
-./ros-docker.sh --dryrun -c -r package /tmp "catkin_make"
-./ros-docker.sh --dryrun -c package /tmp "ls"
-./ros-docker.sh --dryrun -d package /tmp "catkin_make"
+./ros-docker.sh --dryrun package /tmp "catkin_make"
+./ros-docker.sh --dryrun --rosdep package /tmp "catkin_make"
+./ros-docker.sh --dryrun --rosdep --rebuild package /tmp "catkin_make"
+./ros-docker.sh --dryrun --rosdep --rebuild --debug package /tmp "catkin_make"
+
+./ros-docker.sh --dryrun --clean package /tmp "catkin_make"
+./ros-docker.sh --dryrun --clean --rosdep package /tmp "catkin_make"
+./ros-docker.sh --dryrun --clean --rosdep --rebuild package /tmp "catkin_make"
+./ros-docker.sh --dryrun --clean --rosdep --rebuild --debug package /tmp "catkin_make"
+
+./ros-docker.sh -d package /tmp "catkin_make"
+./ros-docker.sh -d -r package /tmp "catkin_make"
+./ros-docker.sh -d -r -R package /tmp "catkin_make"
+./ros-docker.sh -d -r -R -D package /tmp "catkin_make"
+
+./ros-docker.sh -d -c package /tmp "catkin_make"
+./ros-docker.sh -d -c -r package /tmp "catkin_make"
+./ros-docker.sh -d -c -r -R package /tmp "catkin_make"
+./ros-docker.sh -d -c -r -R -D package /tmp "catkin_make"
--- tests/workspacing.sh
+++ tests/workspacing.sh
@@ -3,7 +3,22 @@
 # quit on first error
 set -e
 
-./ros-docker.sh --dryrun -r workspace /tmp "catkin_make"
-./ros-docker.sh --dryrun -c -r workspace /tmp "catkin_make"
-./ros-docker.sh --dryrun -c workspace /tmp "ls"
-./ros-docker.sh --dryrun -d workspace /tmp "catkin_make"
+./ros-docker.sh --dryrun workspace /tmp "catkin_make"
+./ros-docker.sh --dryrun --rosdep workspace /tmp "catkin_make"
+./ros-docker.sh --dryrun --rosdep --rebuild workspace /tmp "catkin_make"
+./ros-docker.sh --dryrun --rosdep --rebuild --debug workspace /tmp "catkin_make"
+
+./ros-docker.sh --dryrun --clean workspace /tmp "catkin_make"
+./ros-docker.sh --dryrun --clean --rosdep workspace /tmp "catkin_make"
+./ros-docker.sh --dryrun --clean --rosdep --rebuild workspace /tmp "catkin_make"
+./ros-docker.sh --dryrun --clean --rosdep --rebuild --debug workspace /tmp "catkin_make"
+
+./ros-docker.sh -d workspace /tmp "catkin_make"
+./ros-docker.sh -d -r workspace /tmp "catkin_make"
+./ros-docker.sh -d -r -R workspace /tmp "catkin_make"
+./ros-docker.sh -d -r -R -D workspace /tmp "catkin_make"
+
+./ros-docker.sh -d -c workspace /tmp "catkin_make"
+./ros-docker.sh -d -c -r workspace /tmp "catkin_make"
+./ros-docker.sh -d -c -r -R workspace /tmp "catkin_make"
+./ros-docker.sh -d -c -r -R -D workspace /tmp "catkin_make"
